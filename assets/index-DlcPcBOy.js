80fe940cbb9cbb3b1699170af6b7403a48132a33
inavdeep13 <inavdeep13@gmail.com>
2026-05-08 14:59:16 +0530

    Updates

--- assets/index-DlcPcBOy.js
+++ assets/index-DlcPcBOy.js
@@ -6,7 +6,7 @@
 
         <div class="flex items-center justify-between h-20">
 
-          <a href="/index.html" class="flex items-center">
+          <a href="./index.html" class="flex items-center">
             <img 
               src="${e}" 
               alt="Nihal Healthcare Logo"
@@ -76,7 +76,7 @@
               </li>
 
               <li>
-                <a href="/certifications.html" class="text-[15px] font-semibold text-slate-700 hover:text-blue-600 transition-all">
+                <a href="./certifications.html" class="text-[15px] font-semibold text-slate-700 hover:text-blue-600 transition-all">
                   Certificates
                 </a>
               </li>
@@ -88,14 +88,14 @@
               </li> -->
 
               <li>
-                <a href="/history.html" class="text-[15px] font-semibold text-slate-700 hover:text-blue-600 transition-all">
+                <a href="./history.html" class="text-[15px] font-semibold text-slate-700 hover:text-blue-600 transition-all">
                   History
                 </a>
               </li>
 
               <li>
                 <a
-                  href="/contact.html"
+                  href="./contact.html"
                   class="text-[15px] font-semibold text-slate-700 hover:text-blue-600 transition-all">                
                   Contact Us
                 </a>
@@ -194,7 +194,7 @@
             </li>
 
             <li>
-              <a href="/certifications.html" class="text-lg font-semibold text-slate-800">
+              <a href="./certifications.html" class="text-lg font-semibold text-slate-800">
                 Certificates
               </a>
             </li>
@@ -212,7 +212,7 @@
             </li> -->
 
             <li>
-              <a href="/history.html" class="text-lg font-semibold text-slate-800">
+              <a href="./history.html" class="text-lg font-semibold text-slate-800">
                 History
               </a>
             </li>
@@ -221,7 +221,7 @@
 
             <li>
 
-              <a href="/contact.html" class="text-lg font-semibold text-slate-800" >
+              <a href="./contact.html" class="text-lg font-semibold text-slate-800" >
                 Contact Us
               </a>
 
