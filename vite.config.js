caba35613b84af20667b48002edf69f37c96cda0
inavdeep13 <inavdeep13@gmail.com>
2026-05-08 15:05:44 +0530

    fix: vite configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+import { resolve } from 'path'
+
 export default defineConfig({
+
   plugins: [tailwindcss()],
+
   base: '/NihalHealthcare/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        history: resolve(__dirname, 'history.html'),
+        certifications: resolve(__dirname, 'certifications.html'),
+      },
+    },
+  },
+
 })
